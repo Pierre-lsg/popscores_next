a9535bd241a6e47459967c6acadc0bb28adf1ae2
Pierre <pierre.bonnafouos@gmail.com>
2026-01-02 17:29:15 +0100

    Updates

---
+++ _app/immutable/chunks/BPzVXLU1.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_14dt505?.base??"/popscores_next",t=globalThis.__sveltekit_14dt505?.assets??s??"";export{t as a,s as b};
--- _app/immutable/chunks/Cv2Q5ktx.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_ee6mv2?.base??"/popscores_next",e=globalThis.__sveltekit_ee6mv2?.assets??s??"";export{e as a,s as b};
