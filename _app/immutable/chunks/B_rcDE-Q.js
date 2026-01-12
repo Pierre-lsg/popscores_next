7d8adc045ff7552009307f7195f3f299fcb34506
Pierre Bonnafous <pierre.bonnafous@gmail.com>
2026-01-12 16:10:30 +0100

    Updates

--- _app/immutable/chunks/B_rcDE-Q.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1n8fl0a?.base??"/popscores_next",a=globalThis.__sveltekit_1n8fl0a?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/C_c8Lkg7.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_783nmm?.base??"/popscores_next",e=globalThis.__sveltekit_783nmm?.assets??s??"";export{e as a,s as b};
