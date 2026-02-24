f233d011bd3b19a780c12e12a24c61465b524528
pierre <pierre.bonnafous@gmail.com>
2026-02-24 23:21:06 +0100

    Enhance ChampionshipRanking component: implement detailed ranking calculations for players and clubs, including support for individual and team competitions, and integrate new data sources for improved scoring accuracy.

--- src/lib/components/championship/competition/ChampionshipRanking.svelte
+++ src/lib/components/championship/competition/ChampionshipRanking.svelte
@@ -4,6 +4,11 @@
 	import '$lib/styles/golfScoring.css';
 	import type { Championship } from '$lib/types/championshipType';
 	import type { Ranking } from '$lib/types/championshipType';
+	import type { Player, RankedPlayer } from '$lib/types/playerType';
+	import type { Course } from '$lib/types/courseType';
+	import type { Target } from '$lib/types/targetType';
+	import type { RankedTeam } from '$lib/types/teamType';
+
 	import { onMount } from 'svelte';
 	import { playersChampionshipStore } from '$lib/stores/championship/playersChampionshipStore.svelte';
 	import { clubsStore } from '$lib/stores/championship/clubsStore.svelte';
@@ -13,8 +18,14 @@
 	import { mpsStore } from '$lib/stores/championship/markedPointScaleStore.svelte';
 	import type { MarkedPointScale } from '$lib/types/markedPointScaleType';
 	import { smartSort } from '$lib/utils/sharedFunction';
-	import RankingCard from '$lib/components/RankingCard.svelte';
+	import { teamsForDoubleRanking } from '$lib/utils/championship/competitionsFunctions.svelte';
+
 	import { teamsChampionshipStore } from '$lib/stores/championship/teamsChampionshipStore.svelte';
+	import { targetsChampionshipStore } from '$lib/stores/championship/targetsChampionshipStore.svelte';
+	import { coursesChampionshipStore } from '$lib/stores/championship/coursesChampionshipStore.svelte';
+	import { getRankedPlayers, getRankedTeams } from '$lib/utils/session/golfScoringFunction.svelte';
+	import { regulationsStore } from '$lib/stores/championship/regulationsStore.svelte';
+	import type { Regulations, Regulation } from '$lib/types/regulationsType';
 
 	let { currentCompetition = $bindable(), championship = $bindable() } = $props<{
 		currentCompetition: Competition;
@@ -26,55 +37,117 @@
 	};
 
 	onMount(() => {
-		// Calcul du classment général par joueur
+		let rankingClv: Ranking[] = [];
 		let rankingIdv: Ranking[] = [];
 		const idvScale: MarkedPointScale | undefined = mpsStore.getScaleById(
 			championship.individualScale
 		);
+		const clvScale: MarkedPointScale | undefined = mpsStore.getScaleById(
+			championship.collectiveScale
+		);
+
 		championship.competitionsId.forEach((cId: string) => {
 			const competition = competitionsStore.find(cId);
-			if (competition && idvScale && !isCompetitionTeam(competition)) {
+			let course: Course | undefined;
+			let targets: Target[] = [];
+			let regulations: Regulations | undefined;
+			let regulation: Regulation | undefined;
+			if (competition && competition.courseId)
+				course = coursesChampionshipStore.find(competition.courseId);
+			if (course) targets = targetsChampionshipStore.list.filter((t) => course.targets);
+			if (competition && competition.regulationsId)
+				regulations = regulationsStore.find(competition.regulationsId);
+			if (regulations) regulation = regulations.regulation;
+
+			// Calcul pour le classement individuel
+			if (competition && idvScale && regulation && !regulation.teamGame) {
 				let players = playersChampionshipStore.list.filter((p) =>
 					competition.playersId.includes(p.id)
 				);
+				let rankedPlayers: RankedPlayer[] = [];
 				// Récupérer le score de chaque joueur lors de la compétition
 				players.forEach((p) => {
 					p.scores = resultsCompetitionStore.find(competition.id, p.id)?.scores ?? {};
 				});
-				players.sort((a, b) => somme(a.scores) - somme(b.scores));
-				for (let i = 0; i < players.length - 1; i++) {
+				if (targets && players) rankedPlayers = getRankedPlayers(players, targets);
+
+				for (let i = 0; i < rankedPlayers.length; i++) {
 					let prevScore: number = 0;
 					// Recherche du joueur dans rankingPlayers
-					const pId = rankingIdv.findIndex((rp: Ranking) => rp.id === players[i].id);
+					const pId = rankingIdv.findIndex((rp: Ranking) => rp.id === rankedPlayers[i].player.id);
 					if (pId !== -1) {
 						prevScore = championship.rankingPlayers[pId].score;
 						rankingIdv.splice(pId, 1);
 					}
 					rankingIdv.push({
-						id: players[i].id,
+						id: rankedPlayers[i].player.id,
 						score: idvScale.points[i] + prevScore
 					});
 				}
-				// Trier le nouveau championship.rankingPlayers
-				rankingIdv = smartSort(rankingIdv, 'score');
 			}
-		});
-		championship.rankingPlayers = rankingIdv;
 
-		// Calcul du classmenet général par équipe
-		let rankingClv: Ranking[] = [];
-		const clvScale: MarkedPointScale | undefined = mpsStore.getScaleById(
-			championship.collectiveScale
-		);
-		championship.competitionsId.forEach((cId: string) => {
-			const competition = competitionsStore.find(cId);
-			// Todo
-			// Prévoir le cas où competition individuelle mais calcul d'un classement par équipe
+			// Calcul pour le classement des clubs si compétition en équipe
 			if (competition && clvScale && isCompetitionTeam(competition)) {
 				let teams = teamsChampionshipStore.list.filter((t) => competition.teamsId.includes(t.id));
+				let players: Player[] | undefined = playersChampionshipStore.list.filter((p) =>
+					teams
+						.map((t) => t.playersId)
+						.flat()
+						.includes(p.id)
+				);
+				let rankedTeams: RankedTeam[] = [];
+				if (targets && teams && players && regulation)
+					rankedTeams = getRankedTeams(teams, targets, players, regulation);
+				// Conserver les 2 meilleures équipes du club
+				// Calculer le score ... todo à corriger
+				for (let i = 0; i < rankedTeams.length; i++) {
+					let prevScore: number = 0;
+					// Recherche du club dans rankingPTeams
+					const pId = rankingClv.findIndex((rp: Ranking) => rp.id === rankedTeams[i].team.clubId);
+					if (pId !== -1) {
+						prevScore = championship.rankingPlayers[pId].score;
+						rankingClv.splice(pId, 1);
+					}
+					rankingClv.push({
+						id: rankedTeams[i].team.clubId || '',
+						score: clvScale.points[i] + prevScore
+					});
+				}
+			}
+
+			// Calcul pour le classement des clubs si compétition solo et calcul résultat équipe
+			if (
+				competition &&
+				clvScale &&
+				!isCompetitionTeam(competition) &&
+				regulations?.doubleRanking
+			) {
+				let teams = teamsForDoubleRanking(competition, targets, regulations);
+				let players: Player[] | undefined = playersChampionshipStore.list.filter((p) =>
+					teams
+						.map((t) => t.playersId)
+						.flat()
+						.includes(p.id)
+				);
+				let rankedTeams: RankedTeam[] = [];
+				if (targets && teams && players && regulation)
+					rankedTeams = getRankedTeams(teams, targets, players, regulation);
+				for (let i = 0; i < rankedTeams.length; i++) {
+					let prevScore: number = 0;
+					const pId = rankingClv.findIndex((rp: Ranking) => rp.id === rankedTeams[i].team.clubId);
+					if (pId !== -1) {
+						prevScore = championship.rankingPlayers[pId].score;
+						rankingClv.splice(pId, 1);
+					}
+					rankingClv.push({
+						id: rankedTeams[i].team.clubId || '',
+						score: clvScale.points[i] + prevScore
+					});
+				}
 			}
 		});
-		championship.rankingClubs = rankingClv;
+		championship.rankingPlayers = smartSort(rankingIdv, 'score', true);
+		championship.rankingClubs = smartSort(rankingClv, 'score', true);
 	});
 </script>
 
@@ -84,11 +157,7 @@
 <p>S'il s'agit d'une compétition par équipe ou solo avec calcul équipe</p>
 <p>si second cas, calculer les équipes cf. fonction teamsForDoubleRanking()</p>
 <p>Pour chaque équipe des clubs qualifiés</p>
-<p>calculer le classement</p>
-<p>Pour chaque équipe du classement</p>
-<p>s'il n'existe pas dans le rankingClub</p>
-<p>---- créer un club avec 0 points</p>
-<p>ajouter les points de son rang suivant collectiveScale</p>
+<p>...</p>
 
 <p>Classement par joueur</p>
 {#each championship.rankingPlayers as p}
