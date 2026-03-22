2d8c2539fa7c6c2317a7347fbdb60c16e9d9841b
Pierre <pierre.bonnafouos@gmail.com>
2026-03-22 19:54:32 +0100

    Updates

--- _app/immutable/entry/start.B6n0wti5.js
+++
@@ -1,1 +0,0 @@
-import{b as s}from"../chunks/E3Ev_XVI.js";import{w as t}from"../chunks/CquK0t9B.js";export{t as load_css,s as start};
---
+++ _app/immutable/entry/start.D5iXD-QI.js
@@ -0,0 +1,1 @@
+import{b as s}from"../chunks/DuL8UVWt.js";import{w as t}from"../chunks/CB4W-fT8.js";export{t as load_css,s as start};
